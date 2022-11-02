d7505d78e437e2111ab87b7f31a805cfbba70025
Timur <z.a.timur@gmail.com>
2022-11-02 12:22:56 -0400

    Update
    New Sprint 3 Modification to Script

--- Scripts/NCIS.py
+++ Scripts/NCIS.py
@@ -1,4 +1,5 @@
-from pickle import TRUE
+import os
+
 import nvdlib
 
 def checkCVE(searchWord):
@@ -8,9 +9,15 @@ def checkCVE(searchWord):
 def readDockerFile(path):
     with open(path,"r") as fi:
         id = []
+        user = []
+        exposed =[]
         for ln in fi:
             if ln.startswith("FROM"):
                 id.append(ln[5:])
+            if ln.startswith("USER"):
+                user.append(ln[5:])
+            if ln.startswith("EXPOSE"):
+                exposed.append(ln[7:])
 
     fFrom = str(id)
   
@@ -35,11 +42,30 @@ def readDockerFile(path):
         size = len(mod_version)
         version = mod_version[:size-4]
     result = software+' '+version
-    print(result)
+
+    if user:
+        print("User running:", user[0])
+    else:
+        print("User is not defined running as root!")
+
+    if exposed:
+        print("Exposed port:", exposed[0])
+    else:
+        print("No exposed ports")
+
+    print("Running software:", result)
+    print("Possible CVE Vulnerabilities:")
     return(result)
 
 
+# os.system("docker history python")
+# os.system("docker history python > HistoryFile.txt ")
+# os.system("docker history --format "{{.ID}}: {{.CreatedBy}}" python --no-trunc > HistoryFile.txt")
+
+ 
 
 # checkCVE('httpd 2.4.50')
 check = readDockerFile("/Users/timurzhunusov/Downloads/vulhub-master/httpd/CVE-2021-42013/Dockerfile")
+# check = readDockerFile("/Users/timurzhunusov/Documents/GitHub/EC601/Scripts/dockerfile_examples/CentosHttpd")
+
 checkCVE(check)
